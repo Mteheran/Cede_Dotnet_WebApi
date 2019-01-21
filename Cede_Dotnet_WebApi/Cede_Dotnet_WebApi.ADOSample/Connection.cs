98324c9062215c37d1eb219637cbe94d605c7bf6
Miguel Teheran <miguelteheran@gmail.com>
2019-01-21 17:38:45 -0500

    ADO.NET Sample

---
+++ Cede_Dotnet_WebApi/Cede_Dotnet_WebApi.ADOSample/Connection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cede_Dotnet_WebApi.ADOSample
+{
+    public class Connection : IConnection
+    {
+        private SqlConnection con { get; set; } = new SqlConnection();
+
+        public Connection()
+        {
+            con.ConnectionString = ConfigurationManager.AppSettings["cnx"].ToString();
+        }
+
+        public DataTable GetInfo(string query)
+        {
+            SqlCommand command = new SqlCommand(query, con);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataTable result = new DataTable();
+
+            con.Open();
+
+            dataAdapter.Fill(result);
+
+            con.Close();
+
+            return result;
+
+        }
+
+        public bool ExecuteQuery(string query)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(query, con);
+
+                con.Open();
+
+                command.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Cede_Dotnet_WebApi/Cede_Dotnet_WebApi.ADOSample/DepartamentoData.cs
@@ -0,0 +1,28 @@
+﻿using Cede_Dotnet_WebApi.ADOSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cede_Dotnet_WebApi.ADOSample
+{
+    public static class DepartamentoData
+    {
+        public static List<Departamento> GetDepartamentos()
+        {
+            IConnection connection = new Connection();
+
+            var data = connection.GetInfo("SELECT Id,NombreDepartamento FROM Departamento");
+
+            List<Departamento> departamentos = new List<Departamento>();
+
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                departamentos.Add(new Departamento() { Nombre = data.Rows[i]["NombreDepartamento"].ToString(), Id = int.Parse(data.Rows[i]["Id"].ToString()) });
+            }
+
+            return departamentos;
+        }        
+    }
+}
